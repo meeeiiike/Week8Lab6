53c1d798c5ad8f76f5e3a479262ecce9471f853b
G00407095@atu.ie <G00407095@atu.ie>
2025-11-05 14:37:15 +0000

    project setup and dependencies added

---
+++ src/main/java/ie/atu/week7lab5/Week7Lab5Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week7lab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7Lab5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week7Lab5Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week7lab5/Week7Lab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week7lab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week7Lab5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
